ffc2d4ef2ec85cceaba865f5935e005a00048ce3
midoabd <mido.abddd@gmail.com>
2026-04-15 21:21:31 -0500

    added env variables to application.properties

--- src/main/java/com/mavmatch/controller/AdminController.java
+++ src/main/java/com/mavmatch/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.mavmatch.controller;
 
+import com.mavmatch.repository.StudentRepository;
 import com.mavmatch.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,9 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @Autowired
+    private StudentRepository studentRepo;
+
     @GetMapping("/dashboard")
     public Map<String, Object> getDashboard() {
         return adminService.getDashboard();
@@ -23,5 +27,11 @@ public class AdminController {
     @GetMapping("/students")
     public List<Map<String, Object>> getAllStudents() {
         return adminService.getAllStudents();
+    }
+
+    @DeleteMapping("/students/{id}")
+    public Map<String, Object> deleteStudent(@PathVariable Long id) {
+        studentRepo.deleteById(id);
+        return Map.of("success", true, "message", "Student " + id + " deleted");
     }
 }
